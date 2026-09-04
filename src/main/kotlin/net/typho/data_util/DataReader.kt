8392b30ed44e7fb955b18dd78487cb20242abd9c
The Typhothanian <thetyphothanian@gmail.com>
2026-09-04 12:36:13 -0600

    added mapRead and mapWrite functions

--- src/main/kotlin/net/typho/data_util/DataReader.kt
+++ src/main/kotlin/net/typho/data_util/DataReader.kt
@@ -1,5 +1,38 @@
 package net.typho.data_util
 
+import java.util.function.Function
+
 fun interface DataReader<T> {
     fun read(input: SingleValueInput): T
+
+    fun <N> mapRead(
+        read: Function<T, N>
+    ): DataReader<N> {
+        val parent = this
+        return object : DataReader<N> {
+            override fun read(input: SingleValueInput): N {
+                return read.apply(parent.read(input))
+            }
+
+            override fun toString(): String {
+                return "$parent mapped with read $read"
+            }
+        }
+    }
+
+    fun <N> mapRead(
+        cls: Class<N>,
+        read: Function<T, N>
+    ): DataReader<N> {
+        val parent = this
+        return object : DataReader<N> {
+            override fun read(input: SingleValueInput): N {
+                return read.apply(parent.read(input))
+            }
+
+            override fun toString(): String {
+                return "$parent mapped to ${cls.name}"
+            }
+        }
+    }
 }
--- src/main/kotlin/net/typho/data_util/DataWriter.kt
+++ src/main/kotlin/net/typho/data_util/DataWriter.kt
@@ -1,5 +1,38 @@
 package net.typho.data_util
 
+import java.util.function.Function
+
 fun interface DataWriter<T> {
     fun write(output: SingleValueOutput, value: T)
+
+    fun <N> mapWrite(
+        write: Function<N, T>
+    ): DataWriter<N> {
+        val parent = this
+        return object : DataWriter<N> {
+            override fun write(output: SingleValueOutput, value: N) {
+                parent.write(output, write.apply(value))
+            }
+
+            override fun toString(): String {
+                return "$parent mapped with write $write"
+            }
+        }
+    }
+
+    fun <N> mapWrite(
+        cls: Class<N>,
+        write: Function<N, T>
+    ): DataWriter<N> {
+        val parent = this
+        return object : DataWriter<N> {
+            override fun write(output: SingleValueOutput, value: N) {
+                parent.write(output, write.apply(value))
+            }
+
+            override fun toString(): String {
+                return "$parent mapped to ${cls.name}"
+            }
+        }
+    }
 }
